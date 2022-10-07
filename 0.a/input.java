f0b6f8053056d2e5ccb665a1ce36170a63602ac0
sachin kumar <sachinmehtab@gmail.com>
2022-10-07 20:56:02 +0530

    start

---
+++ 0.a/input.java
@@ -0,0 +1,7 @@
+class input{
+    public static void main(String[] args) {
+        System.out.println("sachin");
+        System.out.print("sachin kumar");
+        System.out.print("bunty");
+    }
+};
